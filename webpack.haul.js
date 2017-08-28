6bdbabcfcb593bd5b9ed5c748256ec99d1c4b095
dratwas <drapich.piotr@gmail.com>
2017-08-28 22:12:23 +0200

    Add aliases to haul config

--- webpack.haul.js
+++ webpack.haul.js
@@ -1,3 +1,14 @@
-module.exports = ({ platform }) => ({
+const path = require('path');
+
+module.exports = ({ platform, root }, defaults) => ({
   entry: `./src/mobile/index.${platform}.js`,
+  resolve: {
+    ...defaults.resolve,
+    alias: {
+      ...defaults.resolve.alias,
+      shared: path.join(root, 'src', 'shared'),
+      components: path.join(root, 'src', 'mobile', 'components'),
+      containers: path.join(root, 'src', 'mobile', 'containers'),
+    },
+  },
 });
